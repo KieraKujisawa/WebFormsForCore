7f5bbea09d14cb8ee9fe17a61e9100762fb369c1
Kiera Meredith <kiera@meredithkm.info>
2025-11-06 09:59:08 -0500

    Separate WebFormForCoreBuild into exe and two utility libraries

---
+++ src/WebFormsForCore.Build.Designer/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers(PartialTrustVisibilityLevel = PartialTrustVisibilityLevel.NotVisibleByDefault)]
+[assembly: SecurityRules(SecurityRuleSet.Level1, SkipVerificationInFullTrust = true)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: DefaultDllImportSearchPaths(DllImportSearchPath.System32 | DllImportSearchPath.AssemblyDirectory)]
